40602bf60b32b36418e76bc3bc6ab098ee2c59cf
KryLex0 <adam.mkh@hotmail.fr>
2022-03-09 17:29:59 +0100

    Mise à jour des commentaires du code

--- index.php
+++ index.php
@@ -26,8 +26,8 @@ if(!$_GET["numPage"]){
 //connexion a la BDD
 $mysqlClient = new PDO($dbname, $login, $password);
 
+//fonction qui retourne le nombre de lignes dans la BDD
 function getDatabaseCount($mysqlClient){
-    //requete qui retourne le nombre de lignes dans la BDD afin d'afficher des numéros de pages en fonction du nombre d'élements
     $sqlQueryCount = "SELECT COUNT(*) FROM films";
 
     $result = $mysqlClient->prepare($sqlQueryCount);
@@ -39,16 +39,15 @@ function getDatabaseCount($mysqlClient){
 //nombre d'element à afficher par page
 $nbElemPage = 6;
 
-
-
-//permet d'obtenir le nombre de page qui contiendront 6 elements de la BDD par page (48/6 arrondi au supérieur donne 10 pages) 
+//permet d'obtenir le nombre de page qui contiendront 6 elements de la BDD par page (58/6 arrondi au supérieur donne 10 pages de 6 élements max) 
 $nbPageData = round(getDatabaseCount($mysqlClient)/$nbElemPage); 
 
-//offset qui permet d'obtenir les lignes de données à partir du numéro de page (page 2 => élements 7 à 6*2)
-$numDepartElem = $nbElemPage * $_GET['numPage'] - $nbElemPage ;
+//offset qui permet d'obtenir les lignes de données à partir du numéro de page (page 2 => de l'élement 7 à ...)
+$numDepartElem = $nbElemPage * $_GET['numPage'] - $nbElemPage;
 
 function getMovieDatabase($mysqlClient, $nbElemPage, $numDepartElem){
     //requete pour obtenir les X lignes de la BDD en fonction de l'offset (la page ou l'on se trouve)
+    //avec une limite fixé du nombre de résultat à retourner
     $sqlQueryData = "SELECT * FROM films LIMIT $nbElemPage OFFSET $numDepartElem";
 
     $result = $mysqlClient->prepare($sqlQueryData);
@@ -68,6 +67,7 @@ if(!getMovieDatabase($mysqlClient, $nbElemPage, $numDepartElem)){
     }
 }
 
+//fonction qui permet d'obtenir l'url ou l'on se trouve
 function getCurrentUrl($numPage){
     $query = $_GET;
     $query['numPage'] = $numPage;
@@ -75,7 +75,7 @@ function getCurrentUrl($numPage){
     return "?" . $query_result;
 }
 
-
+//fonction qui permet d'obtenir l'url de la page précédente
 function getPreviousPageUrl(){
     $previousPageNumber = $_GET["numPage"] - 1;
     $baseUrl = strtok($_SERVER['REQUEST_URI'], '?');
@@ -83,6 +83,7 @@ function getPreviousPageUrl(){
     return $previousPageUrl;
 }
 
+//fonction qui permet d'obtenir l'url de la page suivante
 function getNextPageUrl(){
     $nextPageNumber = $_GET["numPage"] + 1;
     $baseUrl = strtok($_SERVER['REQUEST_URI'], '?');
@@ -90,6 +91,7 @@ function getNextPageUrl(){
     return $nextPageUrl;
 }
 
+//fonction qui permet d'obtenir une url en fonction du numéro de page passé en paramètre
 function getPageUrlByNumber($numNewPage){
     $baseUrl = strtok($_SERVER['REQUEST_URI'], '?');
     $nextPageUrl = $baseUrl . "?numPage=" . $numNewPage;
@@ -102,6 +104,7 @@ function getPageUrlByNumber($numNewPage){
     <div class="divMovie">
         <?php
         $i=0;
+        //affiche les élements retournés de la requete SQL
         foreach(getMovieDatabase($mysqlClient, $nbElemPage, $numDepartElem) as $tab => $val) {
             $i++;
         ?>
@@ -112,6 +115,7 @@ function getPageUrlByNumber($numNewPage){
             </div>
 
         <?php 
+            //toutes les 2 itérations, revient à la ligne afin d'avoir 2 éléments par ligne
             if($i%2==0){
                 ?>
                 <div style='clear:both'></div>
@@ -128,20 +132,23 @@ function getPageUrlByNumber($numNewPage){
 1, 2, [3], 4, 5, ..., 30-->
     <div class="changePage">
         <div class="changePageButton">
+            <!-- Affiche un bouton qui redirige vers la page 1 lorsqu'on se trouve sur une autre page que la page 1 -->
             <?php if($_GET["numPage"] != 1){?>
                 <a href=<?php echo getPageUrlByNumber(1); ?> class="btn btn-info"><?php echo (1); ?></a>
             <?php } ?>
         <?php
         //if($_GET['numPage'] > 1 && $_GET['numPage'] < $nbPageData){
             for($i=-2; $i<3; $i++){
+                //s'il y a une différence > 2 entre la 1ère page et la page actuelle (ou entre la dernière page et la page actuelle), affiche [...]
                 if($i == -2 && $_GET["numPage"] + $i > 2 || $i == 2 && $_GET["numPage"] + $i < $nbPageData - 2 ){
                     ?><a class="btn btn-info" readonly><?php echo("..."); ?></a><?php
                     
                 }
+                //affiche le numéro de page actuel
                 elseif($i == 0){
                     ?><strong><a class="btn btn-info" style="background-color:grey"><?php echo ($_GET["numPage"] + $i); ?></a></strong><?php
                     
-                
+                //affiche les numéros de pages précédents et suivants autour de la page actuelle
                 }elseif($_GET["numPage"] + $i > 1 && $_GET["numPage"] + $i < $nbPageData ){
         ?>
                 <a href=<?php echo getPageUrlByNumber($_GET["numPage"] + $i); ?> class="btn btn-info"><?php echo ($_GET["numPage"] + $i); ?></a>
@@ -150,6 +157,7 @@ function getPageUrlByNumber($numNewPage){
                 }
             }
         //}
+        //Affiche un bouton qui redirige vers la dernière page lorsqu'on se trouve sur une autre page que la dernière
             if($_GET["numPage"] != $nbPageData){?>
                 <a href=<?php echo getPageUrlByNumber($nbPageData); ?> class="btn btn-info"><?php echo ($nbPageData); ?></a>
             <?php } ?>
@@ -165,27 +173,35 @@ function getPageUrlByNumber($numNewPage){
     <div class="changePage">
         <div class="changePageButton">
         <?php
+        //si on se trouve sur une page autre que la 1ère et dernière
         if($_GET['numPage'] > 1 && $_GET['numPage'] < $nbPageData){
         ?>
+                <!-- Aller à la 1ère page -->
                 <a href=<?php echo getPageUrlByNumber(1); ?> class="btn btn-info"><<</a>
-
+                <!-- Aller à la page précédente -->
                 <a href=<?php echo getPreviousPageUrl(); ?> class="btn btn-info"><</a>
+                <!-- Affiche la page actuelle -->
+                <strong><a class="btn btn-info" style="background-color:grey"><?php echo $_GET["numPage"]; ?></a></strong>
+                <!-- Aller à la page suivante -->
+                <a href=<?php echo getNextPageUrl(); ?> class="btn btn-info">></a>
+                <!-- Aller à la dernière page -->
+                <a href=<?php echo getPageUrlByNumber($nbPageData); ?> class="btn btn-info">>></a>
+
+        <?php
+            //si on se trouve sur la page 1
+            }elseif($_GET['numPage'] == 1){
+        ?>
+                <!-- Affiche seulement les boutons suivant et dernière page -->
                 <strong><a class="btn btn-info" style="background-color:grey"><?php echo $_GET["numPage"]; ?></a></strong>
                 <a href=<?php echo getNextPageUrl(); ?> class="btn btn-info">></a>
                 
                 <a href=<?php echo getPageUrlByNumber($nbPageData); ?> class="btn btn-info">>></a>
 
         <?php
-        }elseif($_GET['numPage'] == 1){
-        ?>
-        <strong><a class="btn btn-info" style="background-color:grey"><?php echo $_GET["numPage"]; ?></a></strong>
-                <a href=<?php echo getNextPageUrl(); ?> class="btn btn-info">></a>
-                
-                <a href=<?php echo getPageUrlByNumber($nbPageData); ?> class="btn btn-info">>></a>
-
-        <?php
+            //si on se trouve sur la dernière page
             }elseif($_GET['numPage'] == $nbPageData){
         ?>
+                <!-- Affiche seulement les boutons précédent et 1ère page -->
                 <a href=<?php echo getPageUrlByNumber(1); ?> class="btn btn-info"><<</a>
 
                 <a href=<?php echo getPreviousPageUrl(); ?> class="btn btn-info"><</a>
